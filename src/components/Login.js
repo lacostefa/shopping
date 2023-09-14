af7b48cd703342e054f7a0f30c23ae7242a28157
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2023-09-15 01:35:12 +0330

    Login page design

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
-const Login=()=>{
-    return(
-        <div>Login</div>
-    )
+const Login = () => {
+    const [log, setLog] = useState([]);
+
+    const changeHandler = (event) => {
+        setLog(event.target.name)
+    }
+    return (<>
+        <div>
+            <label>FirstName</label>
+            <input type="text" name="firstName" onChange={changeHandler}/>
+        </div>
+        <div>
+            <label>LastName</label>
+            <input type="text" onChange={changeHandler}/>
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" onChange={changeHandler}/>
+        </div>
+        <div>
+            <Link to="login">Sign Up</Link>
+        </div>
+    </>)
 }
 
 export default Login;
