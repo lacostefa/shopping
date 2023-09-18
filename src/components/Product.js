c37441b7ed29b9faf8323c4adccafbe3a77f36d4
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2023-09-18 11:07:56 +0330

    create a function for shorten title

---
+++ src/components/Product.js
@@ -0,0 +1,15 @@
+import React,{useContext} from "react";
+
+
+
+const Products = ({image, price,category,title,description}) => {
+
+    const [disptch,state]=useContext(Con)
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Product;
