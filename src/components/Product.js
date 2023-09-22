1fe081ef4a0a52f8cf5f6e947eb9cac86c4717dd
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2023-09-22 12:04:00 +0330

    fix context

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,50 @@
-import React,{useContext} from "react";
+import React, {useContext} from "react";
+import {Link} from "react-router-dom";
 
 
+///Context
+import {CartContext} from "../context/CartContextProvider";
 
-const Products = ({image, price,category,title,description}) => {
+///functions
+import {shorten, isInCart, quantityCounter} from "../helper/functions";
 
-    const [disptch,state]=useContext(Con)
+
+//Icons
+import trashCart from "../icons/trash.svg"
+
+const [state, dispatch] = useContext(CartContext);
+
+const Product = ({productData}) => {
+
     return (
         <div>
+            <div>
+                <img src={productData.image} alt="product"/>
+            </div>
+            <div>
+                <h3>{shorten(productData.title)}</h3>
+            </div>
+            <div>
+                <p>{productData.price} $</p>
+            </div>
+            <div>
+                <Link to={`/product/${productData.id}`}>Details</Link>
+            </div>
+            <div>
+                {quantityCounter(state, dispatch) > 1 &&
+                    <button onClick={() => dispatch({type: "DECREASE", payload: productData})}>-</button>}
 
+                {quantityCounter(state, dispatch) === 1 &&
+                    <button onClick={() => dispatch({type: "REMOVE", payload: productData})}><img src={trashCart}
+                                                                                                  alt="trashCart"/>
+                    </button>}
+
+                {
+                    isInCart(state, productData.id) ?
+                        <button onClick={() => dispatch({type: "INCENSE", payload: productData})}> +</button>
+                        : <button onClick={() => dispatch({type: "ADD-ITEM", payload: productData})}>AddCart</button>
+                }
+            </div>
         </div>
     )
 }
