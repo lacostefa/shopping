661687a7aa738b4ce67c6d79fecb482502307d7f
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2023-09-24 18:30:57 +0330

    Create Component shopCart for checkOut finally

--- src/components/shared/ShopCart.js
+++ src/components/shared/ShopCart.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, {useContext} from "react";
+
+////Components
+import Cart from "./Cart";
+
+///Context
+import {CartContext} from "../../context/CartContextProvider";
 
 const ShopCart = () => {
+    const {state, dispatch} = useContext(CartContext);
+
     return (
         <div>
-
+            <div>
+                {
+                    state.selectedItems.map(item => <Cart key={item.id} data={item}/>)
+                }
+            </div>
+            <div>
+                {
+                    state.itemsCounter > 0 && <div>
+                        <span>Total Items:{state.itemsCounter}</span>
+                        <span>Total Payments:{state.total}</span>
+                    </div>
+                }
+                <div>
+                    <button onClick={() => dispatch({type: "CHECKOUT"})}>CheckOut</button>
+                    <button onClick={() => dispatch({type: "CLEAR"})}>Clear</button>
+                </div>
+            </div>
         </div>
     )
 }
