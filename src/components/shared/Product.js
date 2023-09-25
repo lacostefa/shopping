b9dfa6e716136f212919994ae33b235cb1019342
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2023-09-25 16:47:48 +0330

    edit Component Product for pagination

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -3,14 +3,14 @@ import {Link} from "react-router-dom";
 
 
 ///Context
-import {CartContext} from "../context/CartContextProvider";
+import {CartContext} from "../../context/CartContextProvider";
 
 ///functions
-import {shorten, isInCart, quantityCounter} from "../helper/functions";
+import {shorten, isInCart, quantityCounter} from "../../helper/functions";
 
 
 //Icons
-import trashCart from "../icons/trash.svg"
+import trashCart from "../../icons/trash.svg"
 
 
 const Product = ({productData}) => {
@@ -19,7 +19,7 @@ const Product = ({productData}) => {
     return (
         <div>
             <div>
-                <img src={productData.image} alt="product"/>
+                <img src={productData.image} alt="product" style={{width: "200px"}}/>
             </div>
             <div>
                 <h3>{shorten(productData.title)}</h3>
@@ -31,19 +31,25 @@ const Product = ({productData}) => {
                 <Link to={`/product/${productData.id}`}>Details</Link>
 
                 <div>
-                    {quantityCounter(state, dispatch) > 1 &&
-                        <button onClick={() => dispatch({type: "DECREASE", payload: productData})}>-</button>}
+                    {
+                        quantityCounter(state, dispatch) > 1 &&
+                        <button onClick={() => dispatch({type: "DECREASE", payload: productData})}>-</button>
+                    }
 
-                    {quantityCounter(state, dispatch) === 1 &&
-                        <button onClick={() => dispatch({type: "REMOVE", payload: productData})}><img src={trashCart}
-                                                                                                      alt="trashCart"/>
-                        </button>}
+                    {
+                        quantityCounter(state, dispatch) === 1 &&
+                        <button onClick={() => dispatch({type: "REMOVE-ITEM", payload: productData})}><img
+                            src={trashCart}
+                            alt="trashCart"/>
+                        </button>
+                    }
 
                     {
                         isInCart(state, productData.id) ?
-                            <button onClick={() => dispatch({type: "INCENSE", payload: productData})}> +</button>
+                            <button onClick={() => dispatch({type: "INCENSE", payload: productData})}>+</button>
                             :
-                            <button onClick={() => dispatch({type: "ADD-ITEM", payload: productData})}>AddCart</button>
+                            <button onClick={() => dispatch({type: "ADD-ITEM", payload: productData})}>Add to
+                                Cart</button>
                     }
                 </div>
             </div>
