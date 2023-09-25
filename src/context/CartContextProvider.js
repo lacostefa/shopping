c08f6fd44af5f4cc301bff343e93d9f5a8da8a45
Zahra Motazediyan <zahra.motazediyan2019@gmail.com>
2023-09-25 20:59:32 +0330

    Add default to switch in Component CartContextProvider

--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -1,7 +1,17 @@
 import React, {createContext, useReducer} from "react";
 
 const initialState = {
-    selectedItems: [], itemsCounter: 0, total: 0, checkOut: false
+    selectedItems: [],
+    itemsCounter: 0,
+    total: 0,
+    checkOut: false
+}
+
+
+const sumItems = (items) => {
+    const itemsCounter = items.reduce((total, product) => total + product.quantity, 0);
+    const total = items.reduce((total, product) => total + product.price * product.quantity, 0).toFixed(2);
+    return {itemsCounter, total}
 }
 
 const cartReducer = (state, action) => {
@@ -9,37 +19,53 @@ const cartReducer = (state, action) => {
         case "ADD-ITEM":
             if (!state.selectedItems.find(item => item.id === action.payload.id)) {
                 state.selectedItems.push({
-                    ...action.payload, quantity: 1, checkOut: false
+                    ...action.payload,
+                    quantity: 1,
                 })
                 return {
-                    ...state, selectedItems: [...state.selectedItems]
+                    ...state,
+                    selectedItems: [...state.selectedItems],
+                    checkOut: false,
+                    ...sumItems(state.selectedItems)
                 }
             }
         case "REMOVE-ITEM":
             const newSelectedItems = state.selectedItems.filter(item => item.id !== action.payload.id)
             return {
-                ...state, selectedItems: [...newSelectedItems]
+                ...state,
+                selectedItems: [...newSelectedItems],
+                ...sumItems(newSelectedItems)
             }
         case "INCREASE":
             const indexA = state.selectedItems.findIndex(item => item.id === action.payload.id)
             state.selectedItems[indexA].quantity++
             return {
                 ...state,
+                ...sumItems(state.selectedItems)
             }
         case "DECREASE":
             const indexD = state.selectedItems.findIndex(item => item.id === action.payload.id)
             state.selectedItems[indexD].quality--
             return {
                 ...state,
+                ...sumItems(state.selectedItems)
             }
         case "CHECKOUT":
             return {
-                selectedItems: [], itemsCounter: 0, total: 0, checkOut: true
+                selectedItems: [],
+                itemsCounter: 0,
+                total: 0,
+                checkOut: true
             }
         case "CLEAR":
             return {
-                selectedItems: [], itemsCounter: 0, total: 0, checkOut: false
+                selectedItems: [],
+                itemsCounter: 0,
+                total: 0,
+                checkOut: false
             }
+        default:
+            return initialState
     }
 }
 
@@ -49,11 +75,13 @@ const CartContextProvider = ({children}) => {
 
     const [state, dispatch] = useReducer(cartReducer, initialState);
 
-    return (<div>
-        <CartContext.Provider value={{state, dispatch}}>
-            {children}
-        </CartContext.Provider>
-    </div>)
+    return (
+        <div>
+            <CartContext.Provider value={{state, dispatch}}>
+                {children}
+            </CartContext.Provider>
+        </div>
+    )
 }
 
 export default CartContextProvider
